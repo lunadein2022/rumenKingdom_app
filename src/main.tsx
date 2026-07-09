597f497fde4aae98dbe731a0ccf5a5727be1bfbd
LUNA <lunadein2022@gmail.com>
2026-07-09 22:42:48 +0900

    big update

--- src/main.tsx
+++ src/main.tsx
@@ -8,4 +8,12 @@ import "./styles/components.css";
 import "./styles/home-scene.css";
 import "./styles/modules.css";
 
-// 왕궁 로비/방 이미지를 앱 시작과 동시에 미
+// 왕궁 로비/방 이미지를 앱 시작과 동시에 미리 불러와, 방을 이동할 때 흰 화면 없이
+// 바로 전환되도록 합니다.
+preloadPalaceAssets();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
